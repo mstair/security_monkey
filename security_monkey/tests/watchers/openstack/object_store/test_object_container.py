f88a5c66c2c1a67ae6ca96c4ff39b6392418acd1
Michael Stair <mstair@att.com>
2017-10-05 20:21:32 +0000

    Fix cloudaux test import statement

--- security_monkey/tests/watchers/openstack/object_store/test_object_container.py
+++ security_monkey/tests/watchers/openstack/object_store/test_object_container.py
@@ -22,7 +22,7 @@
 from security_monkey.tests.watchers.openstack import OpenStackWatcherTestCase
 
 from cloudaux.openstack.object_container import get_container_metadata
-from cloudaux.tests.openstack.mock_object_container import mock_get_container_metadata
+from tests.openstack.mock_object_container import mock_get_container_metadata
 
 import mock
 
