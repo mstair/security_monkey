28cc85098cb57bd702137a2c6ebee9b443106f09
Michael Stair <mstair@att.com>
2017-10-05 20:38:02 +0000

    Update import statements for network and object_store unit tests

--- security_monkey/tests/watchers/openstack/network/test_network.py
+++ security_monkey/tests/watchers/openstack/network/test_network.py
@@ -25,5 +25,5 @@ class OpenStackNetworkWatcherTestCase(OpenStackWatcherTestCase):
 
     def pre_test_setup(self):
         super(OpenStackNetworkWatcherTestCase, self).pre_test_setup()
-        from security_monkey.watchers.openstack.network.port import OpenStackNetwork 
+        from security_monkey.watchers.openstack.network.network import OpenStackNetwork 
         self.watcher = OpenStackNetwork(accounts=[self.account.name])
